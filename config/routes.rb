cfd7fb5b16f8d9c5a7781974e7b77b2dfbb26089
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-08-17 05:14:40 +0000

    public2-5コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'shipping_addresses/index'
+    get 'shipping_addresses/edit'
+    get 'shipping_addresses/create'
+    get 'shipping_addresses/update'
+    get 'shipping_addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/check'
+    get 'orders/finish'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/reset'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/check'
+    get 'customers/withdrawal'
+  end
+  namespace :public do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
   devise_for :customers
   devise_for :admins
   devise_for :users
