e2d40bcb164a920643a92c228a038c145aa395ad
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2022-08-18 08:03:11 +0000

    [admin][tentative]oreder-orederdetail

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
+
   def update
+    @orderdetail = OrderDetail.find(params[:id])
+    if @orderdetail.update(orderdetail_params)
+     flash[:notice] = "You have updated order successfully."
+     redirect_to admin_order_detail_path(@orderdetail.id)
+    else
+     render :updatea
+    end
   end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,17 @@
 class Admin::OrdersController < ApplicationController
+
   def show
+    @order =Order.find(params[:id])
   end
-  
+
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+     flash[:notice] = "You have updated order successfully."
+     redirect_to admin_order_path(@order.id)
+    else
+     render :update
+    end
   end
 
 end
