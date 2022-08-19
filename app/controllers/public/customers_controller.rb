102b3431a399ccecf75821ce3fdc6855343e6f4b
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-19 09:30:22 +0000

    会員登録情報の編集機能を実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,20 @@
 class Public::CustomersController < ApplicationController
+
   def show
-    @customer = Customer.find(current_customer.id)
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customer = Customer.find(current_customer.id)
-    # if @customer.update(customer_params)
-    #   redirect_to public_customer_path(@customer.id)
-    # else
-    #   render "edit"
-    # end
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(@customer.id)
+    else
+      render "edit"
+    end
   end
 
   def check
@@ -23,9 +25,8 @@ class Public::CustomersController < ApplicationController
 
   private
 
-  # def customer_params
-  #   params.require(:customer).permit(:title, :body)
-  # end
-
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email)
+  end
 
 end
