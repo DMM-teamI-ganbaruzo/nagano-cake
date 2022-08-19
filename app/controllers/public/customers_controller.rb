c1f73cf71e90a17e74bc30214000806ac58bde9d
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-19 08:53:44 +0000

    会員情報編集画面の大枠を作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,15 @@
 class Public::CustomersController < ApplicationController
   def show
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+    # if @customer.update(customer_params)
+    #   redirect_to public_customer_path(@customer.id)
+    # else
+    #   render "edit"
+    # end
   end
 
   def update
@@ -14,4 +20,12 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal
   end
+
+  private
+
+  # def customer_params
+  #   params.require(:customer).permit(:title, :body)
+  # end
+
+
 end
