433b4509faa57b210139859b76cfad098c505e4d
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2022-08-19 08:58:28 +0000

    [update]orders, order_details

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
+
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to admin_order_detail_path(@order_detail.id)
+    else
+     render :edit
+    end
   end
+
+private
+  def order_detail_params
+    params.require(:item).permit(:order_id, :item_id, :purchase_price, :quantity, :production_status)
+  end
+
 end
