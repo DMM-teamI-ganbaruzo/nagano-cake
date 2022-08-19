075d0600feb79be1f5baeeac0455308953b2b727
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-08-19 04:16:18 +0000

    top,aboutページ作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.order('id DESC').limit(4)
+    # @item = Item.find(params[:id])
+    @item = Item.order('id DESC').limit(4)
+    @items = Item.all
   end
+  
   def about
   end
 end
