d1b731bf06f4bba7e5d24be3895df1dfdd5593b7
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-08-20 13:51:58 +0000

    注文着手1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,15 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new  #(order_params)
+    # @customer = Customer.find(params[:id])
+    # @shipping_addresses = @customer.shipping_addresses
   end
 
   def check
+    # @order = Order.new(order_params)
+    # @order.postal_code = current_customer.postal_code
+    # @order.address = current_customer.address
+    # @order.name = current_customer.first_name + current_customer.last_name
   end
 
   def finish
@@ -16,4 +23,9 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+  
+  def address_params
+    params.require(:order).permit(:name, :address)
+  end
+  
 end
