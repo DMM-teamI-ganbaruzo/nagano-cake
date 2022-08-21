67f8826d02bd376678965171d394d5529bb72038
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-21 03:58:36 +0000

    order_detailsとitemsのリレーションを追加

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,5 +3,6 @@ class OrderDetail < ApplicationRecord
   enum production_status: { cannot_start: 0, waiting_production: 1, creating_production: 2, finish_production: 3 }
 
   belongs_to :order
+  belongs_to :item
 
 end
