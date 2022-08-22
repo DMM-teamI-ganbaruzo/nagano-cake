1df50e5b84dacab8eb7615f944b8abc685d22cda
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-22 14:35:40 +0000

    製作ステータス変更登録機能の検証中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,16 +3,24 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
-     flash[:notice] = "You have updated book successfully."
-     redirect_to admin_order_detail_path(@order_detail.id)
+      redirect_to admin_order_path(@order_detail.order_id)
     else
-     render :edit
+      render :edit
     end
+
+    # @order_detail = OrderDetail.find(params[:id])
+    # if @order_detail.update(order_detail_params)
+    # flash[:notice] = "You have updated book successfully."
+    # redirect_to admin_order_detail_path(@order_detail.id)
+    # else
+    # render :edit
+    # end
   end
 
 private
   def order_detail_params
-    params.require(:item).permit(:order_id, :item_id, :purchase_price, :quantity, :production_status)
+    # params.require(:item).permit(:order_id, :item_id, :purchase_price, :quantity, :production_status)
+    params.require(:order_detail).permit(:production_status)
   end
 
 end
