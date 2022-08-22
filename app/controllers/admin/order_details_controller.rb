ef1c3a3767122444a7160d750cdc372c8acc0376
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-22 15:07:26 +0000

    製作ステータスの更新機能の検討途中2

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,12 +1,30 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order_detail = OrderDetail.find(params[:id])
-    if @order_detail.update(order_detail_params)
-      redirect_to admin_order_path(@order_detail.order_id)
+    order_details = OrderDetail.where(order_id: params[:id])
+    if order_details.update(order_detail_params)
+      redirect_to admin_order_path(params[:id])
     else
       render :edit
     end
+
+
+
+    # order_detail = OrderDetail.find(params[:id])
+    # if order_detail.update(order_detail_params)
+    #   redirect_to admin_order_path(order_detail.order_id)
+    # else
+    #   render :edit
+    # end
+
+
+
+    # @order_details = OrderDetail.where(id: params[:id])
+    # if @order_details.update_all(order_detail_params)
+    #   redirect_to admin_order_path(@order_detail.order_id)
+    # else
+    #   render :edit
+    # end
 
     # @order_detail = OrderDetail.find(params[:id])
     # if @order_detail.update(order_detail_params)
