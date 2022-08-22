042612c5ca33185c6a77070442775391b3f2fd34
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-22 07:15:13 +0000

    会員情報編集機能のテスト中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,5 +12,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render "edit"
+    end
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :delete_status)
+  end
+
 end
